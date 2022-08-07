1fe117bd13bc216294add4096e0346597627f009
Zidane Innis <zidaneinnis@gmail.com>
2022-08-07 15:03:14 -0500

    Finished Building the SidebarOption Component
    -Began building EmailList Component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
+import { Mail } from "@mui/icons-material";
+import { Switch } from "@mui/material";
 import React from "react";
+import { Route, Router } from "react-router-dom";
 import "./App.css";
+import EmailList from "./EmailList";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className="app__body">
+          <Sidebar />
+          <Switch>
+            <Route path="/null">
+              <EmailList />
+              <Mail />
+            </Route>
+          </Switch>
+        </div>
+      </div>
+    </Router>
   );
 }
 
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,17 @@
 import React from "react";
 import "./Sidebar.css";
 import AddIcon from "@mui/icons-material/Add";
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
+import InboxIcon from "@mui/icons-material/Inbox";
+import SidebarOption from "./SidebarOption";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import SendIcon from "@mui/icons-material/Send";
+import InsertDriveFileIcon from "@mui/icons-material/InsertDriveFile";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import PhoneIcon from "@mui/icons-material/Phone";
 function Sidebar() {
   return (
     <div className="sidebar">
@@ -11,6 +21,31 @@ function Sidebar() {
       >
         Compose
       </Button>
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={SendIcon} title="Sent" number={54} />
+      <SidebarOption Icon={InsertDriveFileIcon} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
